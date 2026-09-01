fa7ec4c490ce978f9c80033939cc50f56aab3250
Adridinan Najmi Faza <104964855+najmifaza@users.noreply.github.com>
2026-09-01 12:15:10 +0700

    Update bujurSangkar.java

--- src/BujurSangkar_1/bujurSangkar.java
+++ src/BujurSangkar_1/bujurSangkar.java
@@ -1,10 +1,10 @@
 package BujurSangkar_1;
 
-public class class bujurSangkar{
+public class bujurSangkar{
     int hitungLuas(int sisi){ int luas = sisi * sisi;
         return(luas);
     }
-    int hitungKeliling(int sisi){ int keliling = 4 * luas;
-        return(luas);
+    int hitungKeliling(int sisi){ int keliling = 4 * sisi;
+        return(keliling);
     }
 }
